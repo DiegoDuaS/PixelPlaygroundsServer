5db3ae621de8598f0a621d30e3c2e2a5d53cd60e
DiegoDuaS <dua22075@uvg.edu.gt>
2024-04-28 23:15:47 -0600

    fix: changes for deployment

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3002;
 
 const {posts, createpost, deletepost, getpost, updatePost} = require('./db.js');
 const { generateToken } = require('./jwt.js');
